0255b58a9c62352354bd1ab2d38f974ad2a4b60a
AppKid LLC <contact@appkid.xyz>
2022-05-15 01:50:09 -0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Kaleb Wynne on 5/15/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
